028917e91e771f52f62a5be19a8dababb68a8684
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-10-30 12:41:07 -0400

    correct partition

--- example_odyssey.sh
+++ example_odyssey.sh
@@ -4,10 +4,11 @@
 #SBATCH -n 32
 ### Requested computing time in minutes
 #SBATCH -t 30
+### Partition or queue name
+#SBATCH -p conroy
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=1000
 ### output and error logs
 #SBATCH -o mpitest.out
 #SBATCH -e mpitest.err
-#SBATCH 
 mpirun -n 32 python test_mpi.py
