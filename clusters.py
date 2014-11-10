200e65a07a70cd2658953736c7fbab0afdfe222b
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-11-10 15:11:32 -0500

    make GP a global, modernize model inialization

--- clusters.py
+++ clusters.py
@@ -4,13 +4,17 @@ import time, sys, os
 import numpy as np
 import pickle
 
-from bsfh import sps_basis, modeldef
+from bsfh import sps_basis, sedmodel, modeldef
+from bsfh.gp import GaussianProcess
 import bsfh.fitterutils as utils
 import bsfh.datautils as dutils
 
 #SPS Model as global
 smooth_velocity = False
 sps = sps_basis.StellarPopBasis(smooth_velocity = smooth_velocity)
+
+#Gp as global
+gp = GaussianProcess(None, None)
 
 #LnP function as global
 def lnprobfn(theta, mod):
@@ -27,17 +31,17 @@ def lnprobfn(theta, mod):
         log_mu = np.log(mu)
         #polynomial in the log
         log_cal = (mod.calibration(theta))
-        
         mask = mod.obs['mask']
         d1 = time.time() - t1
 
         # Spectroscopy term
         t2 = time.time()
+        gp.wave, gp.sigma = mod.obs['spectrum'][mask], mod.obs['unc'][mask]
         #use a residual in log space
         delta = (mod.obs['spectrum'] - log_mu - log_cal)[mask]
-        mod.gp.factor(mod.params['gp_jitter'], mod.params['gp_amplitude'],
-                      mod.params['gp_length'], check_finite=False, force=False)
-        lnp_spec = mod.gp.lnlike(delta)
+        gp.factor(mod.params['gp_jitter'], mod.params['gp_amplitude'],
+                  mod.params['gp_length'], check_finite=False, force=False)
+        lnp_spec = gp.lnlike(delta)
         d2 = time.time() - t2
 
         # Photometry term
@@ -76,25 +80,8 @@ except(ValueError):
 if __name__ == "__main__":
 
     inpar = utils.parse_args(sys.argv)
-    rp, parlist = modeldef.read_plist(inpar['param_file'])
-    _, plist_string = modeldef.read_plist(inpar['param_file'], raw_json =True)
-    rp['param_file'] = inpar['param_file']
-    #parlist, rp = modeldef.default_parlist, modeldef.rp
-    #print(type(rp), len(rp))
-    #rp = utils.parse_args(sys.argv, rp = rp)
-    #sys.exit()
-    
-    ############
-    # LOAD DATA
-    ##############
-    if rp['verbose']:
-        print('Loading data')
-
-    obs = dutils.load_obs_mmt(**rp)
-    #ignore the IR magnitudes
-    obs['mags'] = obs['mags'][0:4]
-    obs['mags_unc'] = obs['mags_unc'][0:4]
-    obs['filters'] = obs['filters'][0:4]
+    parset = modeldef.ProspectrParams(inpar['param_file'])
+    rp = parset.run_params
 
     ###############
     # MODEL SET UP
@@ -102,20 +89,35 @@ if __name__ == "__main__":
     if rp['verbose']:
         print('Setting up model')
 
-    model, initial_center = modeldef.initialize_model(rp, parlist, obs)
-    model.theta_desc['spec_norm']['prior_args']['mini'] = 0.0
-    initial_center[model.theta_desc['spec_norm']['i0']] = 0.1
+    model, initial_center = parset.initialize_model(sedmodel.SedModel)
+    #model.params['smooth_velocity'] = smooth_velocity
+    parset.run_params['ndim'] = model.ndim
+    
+    ############
+    # LOAD DATA
+    ##############
+    if rp['verbose']:
+        print('Loading data')
+    if rp.get('mock', False):
+        mock = rp['mock_info']
+        obs = dutils.generate_mock(model, sps, mock)
+        initial_center = model.theta_from_params()
+        initial_center *= np.random.beta(2,2,size=model.ndim)*2.0
+    else:
+        obs = dutils.load_obs_mmt(**rp)
+        #ignore the IR magnitudes
+        obs['mags'] = obs['mags'][0:4]
+        obs['mags_unc'] = obs['mags_unc'][0:4]
+        obs['filters'] = obs['filters'][0:4]
+        
+    modeldef.add_obs_to_model(model, obs, initial_center,
+                              spec=True, phot=True,
+                              logify_spectrum=True,
+                              normalize_spectrum=True,
+                              add_gaussian_process=False)
     
     model.params['smooth_velocity'] = smooth_velocity
-    rp['ndim'] = model.ndim
-
-    #logify the data
-    s, u, m = dutils.logify(model.obs['spectrum'], model.obs['unc'],
-                           model.obs['mask'])
-    model.obs['spectrum'] = s
-    model.obs['unc'] = u
-    model.obs['mask'] = m
-    
+      
     #################
     #INITIAL GUESS USING POWELL MINIMIZATION
     #################
@@ -123,12 +125,8 @@ if __name__ == "__main__":
     if rp['verbose']:
         print('Minimizing')
     ts = time.time()
-
     powell_opt = {'ftol': rp['ftol'], 'xtol':1e-6,
                 'maxfev':rp['maxfev']}
-
-    
-    nthreads = rp['nthreads']
     powell_guesses, pinit = utils.pminimize(chisqfn, model, initial_center,
                                        method ='powell', opts =powell_opt,
                                        pool = pool, nthreads = rp['nthreads'])
@@ -146,7 +144,6 @@ if __name__ == "__main__":
     #sys.exit()
     if rp['verbose']:
         print('emcee...')
-    model.gp.factorized_Sigma=None
     tstart = time.time()
     
     #nsamplers = int(rp['nsamplers'])
@@ -159,10 +156,12 @@ if __name__ == "__main__":
     # PICKLE OUTPUT
     ###################
     results, model_store = {}, {}
+    
     results['run_params'] = rp
     results['obs'] = model.obs
-    results['plist'] = plist_string
-    #results['theta'] = model.theta_desc
+    results['plist'] = parset.model_params
+    results['pardict'] =  modeldef.plist_to_pdict([modeldef.functions_to_names(p)
+                                                  for p in parset.model_params])
     results['initial_center'] = initial_center
     results['initial_theta'] = theta_init
     
@@ -175,7 +174,7 @@ if __name__ == "__main__":
     model_store['powell'] = powell_guesses
     model_store['model'] = model
     #pull out the git hash for bsfh here.
-    bsfh_dir = os.path.dirname(sps_basis.__file__)
+    bsfh_dir = os.path.dirname(modeldef.__file__)
     bgh = utils.run_command('cd ' + bsfh_dir +
                            '\n git rev-parse HEAD')[1][0].replace('\n','')
     cgh = utils.run_command('git rev-parse HEAD')[1][0].replace('\n','')
@@ -185,11 +184,11 @@ if __name__ == "__main__":
     model_store['bsfh_version'] = bgh
     
     tt = int(time.time())
-    out = open('{1}_{0}.sampler{2:02d}_mcmc'.format(tt, rp['outfile'], 1), 'wb')
+    out = open('{1}_{0}_mcmc'.format(tt, rp['outfile']), 'wb')
     pickle.dump(results, out)
     out.close()
 
-    out = open('{1}_{0}.sampler{2:02d}_model'.format(tt, rp['outfile'], 1), 'wb')
+    out = open('{1}_{0}_model'.format(tt, rp['outfile']), 'wb')
     pickle.dump(model_store, out)
     out.close()
     
