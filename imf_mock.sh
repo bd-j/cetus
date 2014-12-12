56a8a00c04fc0c9f0464f60e48bb8f88da711fbd
B Johnson <benjamin.duncan.johnson@gmail.com>
2014-12-12 13:36:19 -0600

    younger mock param file for imf inference

---
+++ imf_mock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+### Name of the job 
+### Requested number of nodes
+#SBATCH -n 64
+### Requested computing time in minutes
+#SBATCH -t 10:00:00
+###partition
+#SBATCH -p normal
+### memory per cpu, in MB
+
+### Job name
+#SBATCH -J 'imf_mock'
+### output and error logs
+#SBATCH -o imfmock_%j.out
+#SBATCH -e imfmock_%j.err
+ibrun python-mpi prospectr.py --param_file=parfiles/imf_young_mock_params.py --nwalkers=126
